815f8f735ee3615970f51ec9990d156c48741632
Ampoi <tange.toshihiro@outlook.jp>
2024-01-25 22:44:48 +0900

    ペーストでファイルを登録できるようにした

--- components/history.vue
+++ components/history.vue
@@ -4,6 +4,10 @@
         :class="{
             'bg-orange-500/30': isBunkasaiYosan,
             'bg-black': !isBunkasaiYosan
+        }"
+        @click="() => {
+            getHistoryImage()
+            showHistoryModal = true
         }">
         <div class="flex flex-col gap-2 grow">
             <div class="flex flex-col">
@@ -12,8 +16,8 @@
                     <span class="text-sm font-normal">({{ history.toolOrArtwork }})</span>
                 </p>
                 <p class="text-xs text-white/60 pl-0.5">{{ history.reason }}</p>
-                </div>
-                <div class="flex flex-row gap-4 pl-1 text-sm text-white/80">
+            </div>
+            <div class="flex flex-row gap-4 pl-1 text-sm text-white/80">
                 <p>
                     <Icon name="bi:currency-yen" class="text-sm -mt-1 text-white/60"/>
                     {{ history.price }}
@@ -26,8 +30,7 @@
                     <Icon name="bi:shop" class="text-sm -mt-1 text-white/60"/>
                     {{ history.shop }}
                 </p>
-                <button
-                    @click="getHistoryImage"
+                <p
                     class="flex flex-row items-baseline"
                     :class="{
                         'text-white/60': !includeHistoryImage,
@@ -35,7 +38,7 @@
                     }">
                     <Icon name="bi:file-earmark-text" class="text-sm -mt-1"/>
                     <p>領収書</p>
-                </button>
+                </p>
                 <div class="flex flex-row gap-1 items-center">
                     <img
                         :src="`https://source.boringavatars.com/marble/120/${history.buyer.id}?colors=610AFA,FA0AF2,B00AFA,0E05FC,FF005C`"
@@ -52,20 +55,70 @@
                 >{{ history.memo }}</p>
             </div>
         </div>
-        <button
-            class="w-12 h-12 rounded-full text-white grid place-content-center text-xl"
-            @click="registerReceiptWithDialog">
-            <Icon name="bi:cloud-upload"/>
-        </button>
     </div>
     <div 
         class="top-0 left-0 fixed w-screen h-screen z-10"
-        v-if="historyImageURL"
-        @click="historyImageURL = undefined">
-        <div class="w-full h-full bg-black/40"/>
-        <img
-            :src="historyImageURL"
-            class="fixed max-w-sm w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        v-if="showHistoryModal">
+        <div
+            class="w-full h-full bg-black/40"
+            @click="showHistoryModal = false"/>
+        <div class="rounded-3xl flex flex-col gap-4 bg-zinc-900 p-4 fixed max-w-xl w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div class="flex flex-row gap-2 text-3xl font-semibold items-baseline">
+                <p>{{ history.purchaceName }}</p>
+                <p class="text-white/30 text-2xl">#{{ history.id }}</p>
+            </div>
+            <div class="flex flex-row gap-4 pl-1 text-sm text-white/80">
+                <p>
+                    <Icon name="bi:currency-yen" class="text-sm -mt-1 text-white/60"/>
+                    {{ history.price }}
+                </p>
+                <p>
+                    <Icon name="bi:calendar" class="text-sm -mt-1 text-white/60"/>
+                    {{ history.date }}
+                </p>
+                <p>
+                    <Icon name="bi:shop" class="text-sm -mt-1 text-white/60"/>
+                    {{ history.shop }}
+                </p>
+                <div class="flex flex-row gap-1 items-center">
+                    <img
+                        :src="`https://source.boringavatars.com/marble/120/${history.buyer.id}?colors=610AFA,FA0AF2,B00AFA,0E05FC,FF005C`"
+                        alt="buyer icon"
+                        class="h-4 w-4 -mt-0.5">
+                    <p>{{ history.buyer.name }}</p>
+                </div>
+                <p
+                    v-if="history.memo"
+                    style="
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;"
+                >{{ history.memo }}</p>
+            </div><!--TODO:ここのUI改善-->
+            <div class="min-h-40 max-h-[400px] overflow-y-auto">
+                <img
+                    v-if="historyImageURL"
+                    :src="historyImageURL"
+                    class="w-full">
+                <div
+                    v-else
+                    class="w-full h-40 border-white/40 bg-zinc-800/40 text-white/40 border-2 border-dotted rounded-xl grid place-content-center">
+                    <div class="flex flex-row gap-4 items-center text-2xl">
+                        <Icon name="bi:receipt"/>
+                        <div
+                            class="flex flex-col"
+                            @paste="registerReceiptWithPaste">
+                            <p>領収書をペースト</p>
+                            <button
+                                class="text-xs"
+                                @click="registerReceiptWithDialog">
+                                またはここをクリックしてファイルを選択
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
@@ -73,6 +126,7 @@ import type { History } from '~/model/history';
 import { getStorage, ref as reference, getDownloadURL, uploadBytes } from "firebase/storage";
 
 const historyImageURL = ref<string>()
+const showHistoryModal = ref(false)
 
 const props = defineProps<{
     history: History
@@ -107,17 +161,22 @@ const registerReceiptWithDialog = () => {
     imageInputElement.addEventListener("change", async () => {
         if( !imageInputElement.files ) throw new Error("ファイルが入力されてません")
         const file = imageInputElement.files[0]
-        if( file && file.type.startsWith("image/") ){
-            registerReceipt(file)
-        }else{
-            console.error("選択された画像はファイルではないです")
-        }
+        registerReceipt(file)
     })
 
     imageInputElement.click()
 }
 
-const registerReceipt = async (file: File) => {
+const registerReceiptWithPaste = (event: ClipboardEvent) => {
+    if( event.clipboardData ){
+        const item = event.clipboardData.items[0]
+        const file = item.getAsFile()
+        registerReceipt(file)
+    }
+}
+
+const registerReceipt = async (file: File | null) => {
+    if( !file || !file.type.startsWith("image/") ) throw new Error("ファイルは画像じゃないです！")
     const reader = new FileReader();
     reader.onload = (e) => {
         const img = new Image();
@@ -139,6 +198,7 @@ const registerReceipt = async (file: File) => {
             await uploadBytes(historyImageRef.value, blob)
 
             emit("updateImages")
+            await getHistoryImage()
         }
 
         if( typeof e.target?.result != "string" ) throw new Error("???")
